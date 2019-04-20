32b19e54b3534dd4aa712165c1de3cb99926cc6c
codereber <jamesmpyle@gmail.com>
2019-04-20 18:36:20 -0400

    update

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,53 @@
+var friendData = require("../data/friends.js")
+
+module.exports = function(app) {
+
+app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  })
+
+  app.post("/api/friends", function(req, res) {
+ var newFriend = req.body;
+ console.log(newFriend)
+ friendData.push(newFriend);
+//  res.json(bestfriend);
+
+
+
+ //compare the current user submitted score with other users
+ // add up current user score array
+ // pull one of my previous users score arrays and add them together
+ // subtract the two numbers to get a compared_score
+ // if compated_score is less then current lowest friend
+var diff;
+var bestdiff = 100;
+var bestfriend = {name:"",photo:""};
+   for( var j = 0; j < friendData.length; j++){
+      diff = 0;
+      
+
+        for (var i = 0; i < friendData[j].scores.length; i++){
+      diff += Math.abs(parseInt(newFriend.scores[i]) - parseInt(friendData[j].scores[i]));
+        }
+      //  console.log(diff);
+      //  console.log(bestdiff);
+        if(diff < bestdiff ){
+          bestfriend.name = friendData[j].name;
+          bestfriend.photo = friendData[j].photo;
+          
+        }
+        
+    }
+    
+   //push newfriend to the friend data array "this is you friends.js array" 
+   //send best friend as json to the client
+   //(on the client side so not on this file lol use the json to populate the popup)
+
+    console.log(bestfriend);
+  })
+}
+  
+
+
+
+
